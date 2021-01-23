113acbf35db1a4299ec6bce32515f990e1706cfc
sohamdutta <xzoham@gmail.com>
2021-01-23 09:51:56 -0500

    s

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div style={{ paddingRight: '300px' }}>
+    <div style={{ paddingLeft: '300px' }}>
       <Navbar />
     </div>
   )
