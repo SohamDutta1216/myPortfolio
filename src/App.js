2d54a15f66b649ecf9723604ca9f8cf5de1ca1a9
sohamdutta <xzoham@gmail.com>
2021-01-23 09:59:46 -0500

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
+
 export default function App() {
+  const size = useWindowSize()
   return (
-    <div style={{ paddingLeft: '300px' }}>
-      <Navbar />
+    <div>
+      {size.width > 1500 ?
+        <div style={{ marginLeft: '20px', marginRight: '20px' }}>
+          <Navbar />
+        </div>
+        :
+        <div>
+          <Navbar />
+        </div>
+
+      }
+
     </div>
   )
 }
